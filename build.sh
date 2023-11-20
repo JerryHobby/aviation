7caec7ebb78a5005b62a7b6956d6350cc9d3d467
Jerry Hobby <Jerry@JerryHobby.com>
2023-11-20 13:44:31 -0600

    ip api test

--- build.sh
+++ build.sh
@@ -1,9 +1,22 @@
 #!/bin/zsh
 git restore package-lock.json
+echo git restore RC= $?
+
 git pull
+echo git pull RC= $?
+
 npm i
+echo npm i RC= $?
+
 npm update
+echo npm update RC= $?
+
 npx prisma generate client
+echo npx prisma generate client RC= $?
+
 npm run build
+echo npm run build RC= $?
+
 pm2 restart aviation
+echo pm2 restart RC= $?
 
