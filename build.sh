9741389c3dd360d2270b28cdf679e80380d7f932
Jerry Hobby <Jerry@JerryHobby.com>
2023-11-22 12:43:28 -0600

    add error handling in build.sh

--- build.sh
+++ build.sh
@@ -1,22 +1,52 @@
 #!/bin/zsh
+
+cd ~/source/aviation
+
+echo ###############################################
 git restore package-lock.json
-echo git restore RC= $?
+if [ $? -ne 0 ]; then
+          echo $? was returned by: git restore package-lock.json.
+          exit
+fi
 
+echo ###############################################
 git pull
-echo git pull RC= $?
+if [ $? -ne 0 ]; then
+          echo $? was returned by: git pull.
+          exit
+fi
 
+echo ###############################################
 npm i
-echo npm i RC= $?
+if [ $? -ne 0 ]; then
+          echo $? was returned by: npm i.
+          exit
+fi
 
+echo ###############################################
 npm update
-echo npm update RC= $?
+if [ $? -ne 0 ]; then
+          echo $? was returned by: npm update.
+          exit
+fi
 
+echo ###############################################
 npx prisma generate client
-echo npx prisma generate client RC= $?
+if [ $? -ne 0 ]; then
+          echo $? was returned by: npx prisma generate client.
+          exit
+fi
 
+echo ###############################################
 npm run build
-echo npm run build RC= $?
+if [ $? -ne 0 ]; then
+          echo $? was returned by: npm run build.
+          exit
+fi
 
+echo ###############################################
 pm2 restart aviation
-echo pm2 restart RC= $?
-
+if [ $? -ne 0 ]; then
+          echo $? was returned by:pm2 restart aviation.
+          exit
+fi
