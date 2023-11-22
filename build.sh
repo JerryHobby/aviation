4462ae543787f9fd537b7aea93c45bd1df2446c7
Jerry Hobby <Jerry@JerryHobby.com>
2023-11-22 12:51:27 -0600

    add error handling in build.sh

--- build.sh
+++ build.sh
@@ -3,50 +3,64 @@
 cd ~/source/aviation
 
 echo "###############################################"
+echo git restore package-lock.json
+
 git restore package-lock.json
 if [ $? -ne 0 ]; then
-          echo $? was returned by: git restore package-lock.json.
+          echo $? was returned.
           exit
 fi
 
 echo "###############################################"
+echo git pull
+
 git pull
 if [ $? -ne 0 ]; then
-          echo $? was returned by: git pull.
+          echo $? was returned.
           exit
 fi
 
 echo "###############################################"
+echo npm i
+
 npm i
 if [ $? -ne 0 ]; then
-          echo $? was returned by: npm i.
+          echo $? was returned.
           exit
 fi
 
 echo "###############################################"
+echo npm update
+
 npm update
 if [ $? -ne 0 ]; then
-          echo $? was returned by: npm update.
+          echo $? was returned.
           exit
 fi
 
 echo "###############################################"
+echo npx prisma generate client
+
 npx prisma generate client
 if [ $? -ne 0 ]; then
-          echo $? was returned by: npx prisma generate client.
+          echo $? was returned.
           exit
 fi
 
 echo "###############################################"
+echo npm run build
+
 npm run build
 if [ $? -ne 0 ]; then
-          echo $? was returned by: npm run build.
+          echo $? was returned.
           exit
 fi
 
 echo "###############################################"
+echo pm2 restart aviation
+
 pm2 restart aviation
 if [ $? -ne 0 ]; then
-          echo $? was returned by:pm2 restart aviation.
+          echo $? was returned.
           exit
 fi
