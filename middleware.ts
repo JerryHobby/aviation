283bbd44da79466fadc4fd0b59879be5e3d8c1e7
Jerry Hobby <Jerry@JerryHobby.com>
2023-11-24 10:36:01 -0600

    added logs

--- middleware.ts
+++ middleware.ts
@@ -9,7 +9,7 @@ export function middleware(request: NextRequest) {
 // See "Matching Paths" below to learn more
 export const config = {
     matcher: [
-        '/admin/:path*',
-        '/users/:id?'
+        '/xxx/:path*',
+        '/xxx/:id?'
     ]
 }
